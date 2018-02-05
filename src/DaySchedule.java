b2c3acaab6902405dcf2589f61dcebdcbf511a46
kobackup <l_mit_ed@hotmail.com>
2018-02-05 10:53:59 -0400

    prior to changing the methods in processRow to check rowTotal to determine whether a previous node already charged for the time period immediately before the current one

--- src/DaySchedule.java
+++ src/DaySchedule.java
@@ -1,32 +1,39 @@
 
 
 import java.util.*;
-
 import java.io.*;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
-public class DaySchedule<String, Time> {
+public class DaySchedule {
 	private final int NUMBUCKETS = 30;
-	private Node<String, Time>[] buckets;	
+	private final int GRACEPERIOD = 10;
+	private final int HOURLYCHARGE = 100;
+	private Node[] buckets;	
+	
 	
 	@SuppressWarnings("unchecked")
 	public DaySchedule()
 	{
-		buckets = (Node<String, Time>[])new Node<?, ?>[NUMBUCKETS];
+		buckets = new Node[NUMBUCKETS];
 	}
 	
-	public void add(String k, Time v)
+	public void add(String k, LocalTime v)
 	{
 		int bucketIndex = hash(k);
 		
 		if(buckets[bucketIndex] != null)
 		{
-			Node<String, Time> ins = new Node<String, Time>(k, v);
+			Node ins = new Node(k, v);
 			ins.setNext(buckets[bucketIndex]);
+			buckets[bucketIndex].setPrevious(ins);
 			buckets[bucketIndex] = ins;			
 		}
 		else
 		{
-			buckets[bucketIndex] = new Node<String, Time>(k, v);
+			buckets[bucketIndex] = new Node(k, v);
 		}
 
 	}	
@@ -35,7 +42,7 @@ public class DaySchedule<String, Time> {
 	{
 		int bucketIndex = hash(code);
 		
-		Node<String, Time> curr = buckets[bucketIndex];
+		Node curr = buckets[bucketIndex];
 		
 		while(curr != null)
 		{
@@ -57,7 +64,7 @@ public class DaySchedule<String, Time> {
 		{
 			int bucketIndex = hash(code);
 			
-			Node<String, Time> curr = buckets[bucketIndex];
+			Node curr = buckets[bucketIndex];
 			
 			while(curr != null)
 			{
@@ -76,37 +83,252 @@ public class DaySchedule<String, Time> {
 		}
 	}
 	
+	
+	
+	public String processRow(String[] line) //maybe return a string[] so the charge can easily be accessed to be totaled
+	{
+		String workstation = line[0];
+		String code = line[1];
+		//String counter = convertCounterName(workstation);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		LocalDateTime dateTime = LocalDateTime.parse(line[2], formatter);		
+		LocalTime loginTime = dateTime.toLocalTime();
+		LocalTime logoffTime = loginTime.plusMinutes(Integer.parseInt(line[3]));
+		
+		//the time span in minutes during which an airline can be 
+		//logged in to check passengers in without being charged a fee
+		int timeAllowed = 180;  
+		
+		//reduce the time allowed to 45 minutes for the gates
+		switch(workstation)
+		{
+		case "GND1GTG001": case "GND1GTG002": case "GND1GTG003": case "GND1GTG004": 
+			timeAllowed = 45;
+		default: break;
+		}
+		
+		
+		if(hasAirlineCode(code))
+		{
+			int bucketIndex = hash(code);
+			
+			Node curr = buckets[bucketIndex];
+			
+			int rowTotal = 0;
+			
+			int charge;
+			
+			if(isOverlappingAny(curr, loginTime, logoffTime, timeAllowed, code))
+			{
+				while(curr != null)
+				{
+					if(curr.getCode().equals(code)) 
+					{
+						/**
+						 * Have an isOverlappingAny() method that first checks the time against all nodes in the chain.
+						 * if the first is true and the nested isOverlappingThis() is false, do nothing in current node.
+						 * if isOverlappingAny() is false, then just charge for the duration without restriction. 
+						 */					
+						
+						if(isOverlapping(loginTime, logoffTime, curr.getTime(), timeAllowed)) //if it's not overlapping, it needs to be checked against other nodes as well
+						{
+							if(!isValidSession(loginTime, logoffTime, curr.getTime(), timeAllowed))
+							{
+								//if()
+								/**
+								 * update the nodes to also keep track of their previous node.
+								 * when checking invalid logged in periods, check only until
+								 * the end of the valid period for the previous node and the 
+								 * beginning of the valid period of the next node, if those exist.
+								 */
+								if(loggedInEarly(loginTime, curr.getTime(), timeAllowed) && loggedOutLate(logoffTime, curr.getTime(), timeAllowed)) //&& rowTotal == 0
+								{
+									LocalTime effectiveLoginTime = loginTime;
+									LocalTime effectiveLogoffTime = logoffTime;
+									
+									if(curr.previous != null)
+									{
+										if(loginTime.compareTo(curr.previous.getTime().plusMinutes(GRACEPERIOD)) < 0) // replace with rowTotal != 0?
+										{
+											effectiveLoginTime = curr.previous.getTime().plusMinutes(GRACEPERIOD); //effectiveLoginTime = curr.getTime().minusMinutes(timeAllowed+GRACEPERIOD)?
+										}									
+									}
+									
+									if(curr.next != null)
+									{
+										if(logoffTime.compareTo(curr.next.getTime().minusMinutes(timeAllowed + GRACEPERIOD)) > 0)
+										{
+											effectiveLogoffTime = curr.next.getTime().minusMinutes(timeAllowed + GRACEPERIOD);
+										}
+									}
+									/**
+									 * if the rowTotal is not zero, that means I already accounted for some of the charge 
+									 * and I probably shouldn't add a charge for anything before the current valid time in the 
+									 * current node because this should have been charged in the previous node.
+									 * The last node (where next is null) should maybe check against midnight.
+									 */
+									rowTotal += (int)effectiveLoginTime.until(curr.getTime().minusMinutes(timeAllowed+GRACEPERIOD), ChronoUnit.MINUTES);
+									rowTotal += (int)curr.getTime().plusMinutes(GRACEPERIOD).until(effectiveLogoffTime, ChronoUnit.MINUTES);
+								}
+								
+								
+								
+								else if(loggedInEarly(loginTime, curr.getTime(), timeAllowed))
+								{
+									LocalTime effectiveLoginTime = loginTime;
+									
+									if(curr.previous != null)
+									{
+										if(loginTime.compareTo(curr.previous.getTime().plusMinutes(GRACEPERIOD)) < 0) // replace with rowTotal != 0?
+										{
+											effectiveLoginTime = curr.previous.getTime().plusMinutes(GRACEPERIOD); //effectiveLoginTime = curr.getTime().minusMinutes(timeAllowed+GRACEPERIOD)?
+										}									
+									}
+									rowTotal += (int)effectiveLoginTime.until(curr.getTime().minusMinutes(timeAllowed+GRACEPERIOD), ChronoUnit.MINUTES);
+									
+								}
+								
+								
+								
+								else if(loggedOutLate(logoffTime, curr.getTime(), timeAllowed))
+								{
+									LocalTime effectiveLogoffTime = logoffTime;
+									
+									if(curr.next != null)
+									{
+										if(logoffTime.compareTo(curr.next.getTime().minusMinutes(timeAllowed + GRACEPERIOD)) > 0)
+										{
+											effectiveLogoffTime = curr.next.getTime().minusMinutes(timeAllowed + GRACEPERIOD);
+										}
+									}
+									rowTotal += (int)curr.getTime().plusMinutes(GRACEPERIOD).until(effectiveLogoffTime, ChronoUnit.MINUTES);
+								}
+							}						
+						}
+					}
+					curr = curr.getNext();
+				}
+				charge = ((rowTotal/60)+1)*HOURLYCHARGE;
+				// create string array and return it
+			}
+			else //logged-in time does not intersect with any valid period
+			{
+				charge = ((int)(loginTime.until(logoffTime, ChronoUnit.MINUTES))/60 +1)*HOURLYCHARGE;
+				// create string array and return it
+			}
+			
+			
+		}
+		else
+		{
+			System.out.println("There are no records with that code");
+		}
+				
+		return "";
+	}
+	
+	
+	public boolean isOverlappingAny(Node node, LocalTime login, LocalTime logoff, int timeAllowed, String code)
+	{
+		Node curr = node;		
+		while(curr != null)
+		{
+			if(curr.getCode().equals(code) && isOverlapping(login, logoff, curr.getTime(), timeAllowed)) 
+			{
+				return true;
+			}
+			curr = curr.next;
+		}
+				
+		return false;
+	}
+	
+	
+	
+	/**
+	 * Checks whether a given time period has any overlap with 
+	 * any scheduled period for that airline on that day of week
+	 * @param login
+	 * @param logoff
+	 * @param scheduled
+	 * @param timeAllowed
+	 * @return
+	 */
+	public boolean isOverlapping(LocalTime login, LocalTime logoff, LocalTime scheduled, int timeAllowed)
+	{
+		return (login.isBefore(scheduled.plusMinutes(GRACEPERIOD)) && scheduled.minusMinutes(timeAllowed+GRACEPERIOD).isBefore(logoff));
+	}
+	
+	
+	public boolean isValidSession(LocalTime login, LocalTime logoff, LocalTime scheduled, int timeAllowed)
+	{
+		return (login.isAfter(scheduled.minusMinutes(timeAllowed+GRACEPERIOD)) && logoff.isBefore(scheduled.plusMinutes(GRACEPERIOD)));
+	}
+	
+	
+	public boolean loggedInEarly(LocalTime login, LocalTime scheduled, int timeAllowed)
+	{
+		return (login.isBefore(scheduled.minusMinutes(timeAllowed+GRACEPERIOD)));
+	}
+	
+	public boolean loggedOutLate(LocalTime logoff, LocalTime scheduled, int timeAllowed)
+	{
+		return (logoff.isAfter(scheduled.plusMinutes(GRACEPERIOD)));
+	}
+	
+	
+	/**
+	 * hash function used to assign airline codes
+	 * to buckets in the DaySchedule hash table
+	 * @param airlineCode
+	 * @return
+	 */
 	private int hash(String airlineCode)
 	{
 		return Math.abs(airlineCode.hashCode()) % NUMBUCKETS;
 	}
 	
 	
-	private static class Node<String, Time>{
-		private Time time;	
+	/**
+	 * Linked list node representing a single
+	 * scheduled flight within a day's schedule.
+	 * Each stores the scheduled departure time 
+	 * and the relevant airline's code.
+	 * @author Khari
+	 *
+	 */
+	private static class Node {
+		private LocalTime time;	
 		private String code;
-		private Node<String, Time> next;
+		private Node next;
+		private Node previous;
 
 		
 		// Constructor to initialise the Node
-		private Node(String newCode, Time newTime)
+		private Node(String newCode, LocalTime newTime)
 		{
 			time = newTime;
 			next = null;
+			previous = null;
 			code = newCode;
 		}
 
-		private Node<String, Time> getNext() 
+		private Node getNext() 
 		{
 			return next;
 		}
 
-		private void setNext(Node<String, Time> next) 
+		private void setNext(Node next) 
 		{
 			this.next = next;
 		}
+		
+		private void setPrevious(Node previous)
+		{
+			this.previous = previous;
+		}
 
-		private Time getTime() 
+		private LocalTime getTime() 
 		{
 			return time;
 		}
--- src/Init.java
+++ src/Init.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
@@ -19,12 +20,13 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Init {
-	private Set airlineSet;
-	private String dataFilepath;
-	private String scheduleFilepath;
-	private int lastRow;
+	private Set<String> airlineSet; // a set for all airline codes that appear in the month's report
+	private String dataFilepath; // filepath of the SITA report
+	private String scheduleFilepath; // filepath of the flight schedule
+	private int lastRow; // the last row that contains schedule info
+	private String report; // the resulting billing report
 	
-	private DaySchedule[] schedules;
+	private DaySchedule[] schedules; // array of daily schedules based on the schedule file
 	
 	/*
 	private DaySchedule monday;
@@ -38,17 +40,21 @@ public class Init {
 	
 	public Init(String data, String schedule)
 	{
-		airlineSet = new HashSet();
+		airlineSet = new HashSet<String>();
 		dataFilepath = data;
 		scheduleFilepath = schedule;	
 		schedules = new DaySchedule[7];
 		initAirlineSet();
 		initDaySchedules();
 		
-		schedules[6].printTimesForCode("AA");
+		schedules[6].printTimesForCode("LI");
+		
+		processCharges();
 	}
 	
-	
+	/**
+	 * Reads in the SITA report and adds the airline codes to the set
+	 */
 	private void initAirlineSet()
 	{
 		try
@@ -63,10 +69,8 @@ public class Init {
 				if(tempArray[1].length() < 4)
 				{
 					airlineSet.add(tempArray[1]);
-				}
-				
-			} 
-			
+				}				
+			} 			
 			scanner.close();
 			reader.close();
 		} 
@@ -82,6 +86,10 @@ public class Init {
 	}
 	
 	
+	/**
+	 * Reads in the schedule file and adds each scheduled departure to 
+	 * its corresponding DaySchedule
+	 */
 	private void initDaySchedules()
 	{
 		setLastRow();
@@ -112,51 +120,24 @@ public class Init {
 				            {
 			            		String[] entry = nextRow.getCell(i).toString().split(" +");
 			            		String airlineCode = entry[0];
-			            		LocalTime dTime;
-			            		
-			            		
-			            		
+			            		LocalTime dTime;           		
 			            		
 			            		System.out.print(airlineCode + " ");
 
 			            		Matcher match = Pattern.compile("\\(([^)]+)\\)").matcher(nextRow.getCell(i).toString());
-			            	     while(match.find()) {        	    	 
-					             	 String rawTimeString = match.group(1).toLowerCase();
-			            	    	 String dTimeString = LocalTime.parse(rawTimeString,  DateTimeFormatter.ofPattern("h:mma")).format(DateTimeFormatter.ofPattern("HH:mm"));
-			            	    	 dTime = LocalTime.parse(dTimeString);
-			            	    	 System.out.println(dTime);    
-			            	    	 schedules[dayIterator].add(airlineCode, dTime);
+		            	    	while(match.find()) 
+		            	     	{        	    	 
+		            	    		String rawTimeString = match.group(1).toLowerCase();
+		            	    		String dTimeString = LocalTime.parse(rawTimeString,  
+		            	    				DateTimeFormatter.ofPattern("h:mma")).format(DateTimeFormatter.ofPattern("HH:mm"));
+		            	    		dTime = LocalTime.parse(dTimeString);
+		            	    		System.out.println(dTime);    
+		            	    		schedules[dayIterator].add(airlineCode, dTime);
 			            	    	 
-			            	     }
+		            	     	}
 				            }
 			            }
-			            
-			            
-			            
-			            
-			            /*
-			            while (cellIterator.hasNext()) {
-			                Cell cell = cellIterator.next(); 
-		
-			                switch (cell.getCellTypeEnum()) {
-			                    case STRING:
-			                        System.out.print(cell.getStringCellValue());
-			                        break;
-			                    case BOOLEAN:
-			                        System.out.print(cell.getBooleanCellValue());
-			                        break;
-			                    case NUMERIC:
-			                        System.out.print(cell.getNumericCellValue());
-			                        break;			                        
-			                        
-			                }
-			                
-			                
-			                System.out.print(" - ");
-			            }
-			            */
-			            
-			           // System.out.println();
+			
 			        }
 			        dayIterator++;
 		        }
@@ -171,6 +152,11 @@ public class Init {
 		
 	}
 	
+	
+	/**
+	 * Finds the row that begins with the string 'total' and sets lastRow
+	 * to that row number
+	 */
 	private void setLastRow()
 	{
 		try
@@ -178,10 +164,8 @@ public class Init {
 			FileInputStream inputStream = new FileInputStream(new File(scheduleFilepath));
 	        
 	        Workbook workbook = new XSSFWorkbook(inputStream);
-	        Sheet firstSheet = workbook.getSheetAt(0);
-	        
+	        Sheet firstSheet = workbook.getSheetAt(0);       
 	         
-	        
         	Iterator<Row> iterator = firstSheet.iterator();
 	        while (iterator.hasNext()) 
 	        {
@@ -193,9 +177,7 @@ public class Init {
 		            
 		            if(contents.startsWith("TOTAL"))
 		            {
-		            	//System.out.println(contents);
 		            	lastRow = nextRow.getRowNum();
-		            	//System.out.println(lastRow);
 		            	break;
 		            }
 	            }            
@@ -209,6 +191,109 @@ public class Init {
 		 {
 			 e.printStackTrace();
 		 }
+		
+	}
+	
+	/**
+	 * takes the workstation name as input and 
+	 * returns the corresponding counter name.
+	 * @param workstation
+	 * @return counter name
+	 */
+	private String convertCounterName(String workstation) // move this to DaySchedule
+	{
+		switch(workstation)
+		{
+		case "GND1CKB001": case "GND1CKR002":
+			return "Counter 1";
+		case "GND1CKB003": case "GND1CKR004":
+			return "Counter 2";
+		case "GND1CKB005": case "GND1CKR006":
+			return "Counter 3";
+		case "GND1CKB007": case "GND1CKR008":
+			return "Counter 4";
+		case "GND1CKB013": case "GND1CKR014":
+			return "Counter 7";
+		case "GND1CKB017": case "GND1CKR018":
+			return "Counter 9";
+		case "GND1CKB023": case "GND1CKR024":
+			return "Counter 12";
+		case "GND1CKR010":
+			return "Counter 5";
+		case "GND1CKR012":
+			return "Counter 6";
+		case "GND1CKR016":
+			return "Counter 8";
+		case "GND1CKR020":
+			return "Counter 10";
+		case "GND1CKR022":
+			return "Counter 11";
+		case "GND1CKR026":
+			return "Counter 13";
+		case "GND1GTG001":
+			return "Gate 1";
+		case "GND1GTG002":
+			return "Gate 2";
+		case "GND1GTG003":
+			return "Gate 3";
+		case "GND1GTG004":
+			return "Gate 4";
+		default:
+			return "Invalid workstation";
+		}
+		
+	}
+	
+	
+	
+	private void processCharges()
+	{
+		report = "";
+		StringBuilder builder = new StringBuilder(report);
+		for(String code : airlineSet)
+		{
+			try
+			{
+				FileReader reader = new FileReader(dataFilepath);
+				Scanner scanner = new Scanner(reader);
+				scanner.nextLine();
+				
+				while(scanner.hasNextLine())
+				{
+					String line = scanner.nextLine();
+					String[] tempArray = line.split(",");
+					if(tempArray[1].equals(code)) 
+					{
+						//DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+						//LocalDateTime dateTime = LocalDateTime.parse(tempArray[2], formatter);
+						//int dayOfWeek = dateTime.getDayOfWeek().ordinal();
+						//schedules[dayOfWeek].processRow(code, tempArray);
+						
+//						for(String cell : tempArray)
+//						{
+//							System.out.print(cell + "     ");
+//						}
+//						System.out.println();
+						
+						String workstation = tempArray[0];
+						String counter = convertCounterName(workstation);
+						DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy H:mm");
+						LocalDateTime dateTime = LocalDateTime.parse(tempArray[2], formatter);
+						int dayOfWeek = dateTime.getDayOfWeek().ordinal();
+						LocalTime loginTime = dateTime.toLocalTime();
+						LocalTime logoffTime = loginTime.plusMinutes(Integer.parseInt(tempArray[3]));
+					}					
+					
+				} 
+				
+				scanner.close();
+				reader.close();
+			} 
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			} 
+		}		
 		
 	}
 
